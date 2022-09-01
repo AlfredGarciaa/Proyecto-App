ed49bfa5d90ae1c2886789d729ee82b9e6210e96
Alfred <alfred.garcia@ucb.edu.bo>
2022-09-01 07:35:55 -0400

    Ingresar ingreso y descripcion.- adiciona el monto en Bs. y su nombre.

--- src/presenter.js
+++ src/presenter.js
@@ -8,7 +8,9 @@ const balances = document.querySelector("#balance-general");
 const gastos = document.querySelector("#gasto-general");
 const ingresos = document.querySelector("#ingreso-general");
 const gastos_number = document.querySelector("#gasto-monto");
-const gastos_text = document.querySelector("#gasto-descripcion");
+const gastos_string = document.querySelector("#gasto-descripcion");
+const ingresos_number = document.querySelector("#ingreso-monto");
+const ingresos_string = document.querySelector("#ingreso-descripcion");
 
 const form = document.querySelector("#principal-form");
 const div = document.querySelector("#resultado-div");
@@ -19,12 +21,22 @@ form.addEventListener("submit", (event) => {
   const balance_resultado = Number.parseInt(balances.value);
   const gasto_resultado = Number.parseInt(gastos.value);
   const ingreso_resultado = Number.parseInt(ingresos.value);
+
   const gasto_monto = Number.parseInt(gastos_number.value);
+  const gasto_text = gastos_string.value;
+
+  const ingreso_monto = Number.parseInt(ingresos_number.value);
+  const ingreso_text = ingresos_string.value;
 
   div.innerHTML = "<p>Balance: Bs. " + balance(balance_resultado) + "<p>" +
                   "<p>Gastos: Bs. " + gasto(gasto_resultado) + "<p>" +
                   "<p>Ingresos: Bs. " + ingreso(ingreso_resultado) + "<p>" +
+
                   "<p>Monto: Bs." + añadir_monto(gasto_monto) + "<p>" +
-                  "<p>Descripcion: " + añadir_descripcion(gasto_monto) + "<p>" +
+                  "<p>Descripcion: " + añadir_descripcion(gasto_text) + "<p>" +
+
+                  "<p>Monto: Bs." + añadir_monto(ingreso_monto) + "<p>" +
+                  "<p>Descripcion: " + añadir_descripcion(ingreso_text) + "<p>" +
+
                   "</p>";
 });
