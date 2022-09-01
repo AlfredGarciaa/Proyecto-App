798d25f83e94d6d8784661ce7651b8689a170336
Alfred <alfred.garcia@ucb.edu.bo>
2022-09-01 07:07:51 -0400

    Mostrar gastos totales.- inicialmente tendria 0 bs. hasta añadir.

---
+++ src/gasto_general.js
@@ -0,0 +1,7 @@
+function gasto(total) {
+    total = 0;
+  
+    return total;
+  }
+  
+  export default gasto;
--- src/presenter.js
+++ src/presenter.js
@@ -1,6 +1,8 @@
 import balance from "./balance_general.js";
+import gasto from "./gasto_general.js";
 
 const balances = document.querySelector("#balance-general");
+const gastos = document.querySelector("#gasto-general");
 const form = document.querySelector("#principal-form");
 const div = document.querySelector("#resultado-div");
 
@@ -8,6 +10,9 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const balance_resultado = Number.parseInt(balances.value);
+  const gasto_resultado = Number.parseInt(gastos.value);
 
-  div.innerHTML = "<p>Balance: Bs. " + balance(balance_resultado) + "</p>";
+  div.innerHTML = "<p>Balance: Bs. " + balance(balance_resultado) + 
+                  "<p>Gastos: Bs. " + gasto(gasto_resultado) +
+                  "</p>";
 });
