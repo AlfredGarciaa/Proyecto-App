ad42ef0e5558096c0825d82a337a161ff90eb1d1
Alfred <alfred.garcia@ucb.edu.bo>
2022-09-01 07:12:03 -0400

    Mostrar ingresos totales.- inicialmente tendria 0 bs. hasta añadir.

---
+++ src/ingreso_general.js
@@ -0,0 +1,7 @@
+function ingreso(total) {
+    total = 0;
+  
+    return total;
+  }
+  
+  export default ingreso;
--- src/presenter.js
+++ src/presenter.js
@@ -1,8 +1,10 @@
 import balance from "./balance_general.js";
 import gasto from "./gasto_general.js";
+import ingreso from "./ingreso_general.js";
 
 const balances = document.querySelector("#balance-general");
 const gastos = document.querySelector("#gasto-general");
+const ingresos = document.querySelector("#ingreso-general");
 const form = document.querySelector("#principal-form");
 const div = document.querySelector("#resultado-div");
 
@@ -11,8 +13,10 @@ form.addEventListener("submit", (event) => {
 
   const balance_resultado = Number.parseInt(balances.value);
   const gasto_resultado = Number.parseInt(gastos.value);
+  const ingreso_resultado = Number.parseInt(ingresos.value);
 
   div.innerHTML = "<p>Balance: Bs. " + balance(balance_resultado) + 
                   "<p>Gastos: Bs. " + gasto(gasto_resultado) +
+                  "<p>Ingresos: Bs. " + ingreso(ingreso_resultado) +
                   "</p>";
 });
